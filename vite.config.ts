5cc1623bfb2bbde6c773f310ac67a06850a976e0
Raymond Huff <raymond.e.huff2.mil@swf.army.mil>
2024-01-19 16:19:09 -0600

    setup vitest and dev dependancies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+  publicDir: 'public',
+  server: {
+    watch: {
+        ignored: ['**/node_modules/**', '**/dist/**', '**/public/**'],
+    },
+  },
 })
